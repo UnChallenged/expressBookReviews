5a63faf9caee90c53d75983a40bc555f1ef19777
Khizar Murad <123hashinclude@gmail.com>
2023-05-16 00:36:01 +0500

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -15,15 +15,85 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res.status(400).json({ message: "Username and password are required" });
+    }
+    const user = users.find(user => user.username === username && user.password === password);
+    if (!user) {
+      return res.status(401).json({ message: "Invalid username or password" });
+    }
+    const token = jwt.sign({ username: user.username }, 'fingerprint_customer');
+    return res.status(200).json({ token });
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const { review } = req.body;
+  if (!review) {
+    return res.status(400).json({ message: "Review is required" });
+  }
+  let decodedToken;
+  try {
+    const token = req.headers.authorization?.split(' ')[1];  
+    decodedToken = jwt.verify(token, 'fingerprint_customer');
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+  const { username } = decodedToken;
+  const isbn = req.params.isbn;
+  if (!books[isbn]) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  if (!books[isbn].reviews) {
+    books[isbn].reviews = {};
+  }
+  const userReviewIndex = Object.values(books[isbn].reviews).findIndex(r => r.username === username);
+  if (userReviewIndex >= 0) {
+    const reviewKey = `review ${userReviewIndex + 1}`;
+    books[isbn].reviews[reviewKey].review = review;
+  } else {
+    const newReviewKey = `review ${Object.keys(books[isbn].reviews).length + 1}`;
+    books[isbn].reviews[newReviewKey] = { username, review };
+  }
+  return res.status(200).json({ message: "Review saved successfully" });
+
 });
+
+// delete a book review
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    let decodedToken;
+    try {
+      const token = req.headers.authorization?.split(' ')[1];  
+      decodedToken = jwt.verify(token, 'fingerprint_customer');
+    } catch (error) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+    const { username } = decodedToken;
+    
+    if (!books[isbn]) {
+      return res.status(404).json({ message: `Book with ISBN ${isbn} not found` });
+    }
+    
+    if (!books[isbn].reviews) {
+      return res.status(404).json({ message: `No reviews found for book with ISBN ${isbn}` });
+    }
+    
+    // delete all reviews from the specified user
+    let reviews = books[isbn].reviews;
+    Object.keys(reviews).forEach((key) => {
+        if (reviews[key].username === username) {
+          delete reviews[key];
+          isDeleted = true;
+        }
+      });
+    
+    return res.json({ message: "Reviews deleted successfully" });
+});
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
