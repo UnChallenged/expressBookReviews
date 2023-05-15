1dd1348cad65cb2bdd9745ab833ef1f78ac3b667
Khizar Murad <123hashinclude@gmail.com>
2023-05-16 00:35:40 +0500

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,18 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    const token = req.headers.authorization?.split(' ')[1];
+    if (!token) {
+      return res.status(401).json({ message: 'Authentication required.' });
+    }
+  
+    try {
+      const decoded = jwt.verify(token, "fingerprint_customer");
+      req.username = decoded.username;
+      next();
+    } catch (err) {
+      return res.status(401).json({ message: 'Invalid or expired token.' });
+    }
 });
  
 const PORT =5000;
